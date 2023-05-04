bcc5094c0385de60395eafb7bba4923317f252f3
Nayim Hossen <nayimh2015@gmail.com>
2023-05-04 22:33:56 +0600

    nullable_unknown

---
+++ src/nullable_unknown.ts
@@ -0,0 +1,35 @@
+const searchName = (value: string | null) => {
+  if (value === null) {
+    console.log(`There is nothing to search`);
+  } else {
+    console.log("Searching");
+  }
+};
+
+searchName(null);
+
+//kmh^-1 ms^-1
+
+const getMyCarSpeed = (speed: unknown) => {
+  if (typeof speed === "number") {
+    const convertedSpeed = (speed * 1000) / 3600;
+    console.log(`My speed ${convertedSpeed}`);
+  }
+  if (typeof speed === "string") {
+    const [value, unit] = speed.split(` `);
+    const convertedSpeed = (parseFloat(value) * 1000) / 3600;
+    console.log(`My speed is ${convertedSpeed}`);
+  } else {
+    console.log(`there is wrong type`);
+  }
+};
+
+getMyCarSpeed(19);
+getMyCarSpeed("10 kmh");
+getMyCarSpeed(true);
+
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+throwError("Vul korsen vai");
