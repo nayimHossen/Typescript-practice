dc98c293d4079342dbb9acf2bdb48a077791662d
Nayim Hossen <nayimh2015@gmail.com>
2023-05-04 22:10:36 +0600

    union_intersection

---
+++ src/union_intersection.ts
@@ -0,0 +1,33 @@
+type NoobDeveloper = {
+  name: string;
+};
+
+// type JuniorDeveloper = {
+//   name: string;
+//   expertise: string;
+//   experience: number;
+// };
+
+type JuniorDeveloper = NoobDeveloper & {
+  expertise: string;
+  experience: number;
+};
+
+type NextLevelDeveloper = JuniorDeveloper & {
+  leadershipExperience: number;
+  level: "junior" | "mid" | "senior";
+};
+
+const newDeveloper: NoobDeveloper | JuniorDeveloper = {
+  name: "Nayim",
+  experience: 2,
+  expertise: "javascript",
+};
+
+const developer: NextLevelDeveloper = {
+  name: "Manna",
+  experience: 5,
+  expertise: "Typescript",
+  leadershipExperience: 3,
+  level: "senior",
+};
