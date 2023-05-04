efa897ce1ed8631ac473fa3d1019adab2e629dd1
Nayim Hossen <nayimh2015@gmail.com>
2023-05-04 22:46:03 +0600

    ternary, Nullish coeslancing operator

---
+++ src/question.ts
@@ -0,0 +1,43 @@
+//ternary operator
+
+const age: number = 22;
+
+if (age >= 18) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+const isAdult = age >= 18 ? "Yes" : "No";
+console.log(isAdult);
+
+//Null and Undefined
+
+const isAuthenticatedUser = "";
+const userName = isAuthenticatedUser ?? "Guest";
+const userName2 = isAuthenticatedUser ? isAuthenticatedUser : "Guest";
+console.log(userName);
+console.log(userName2);
+
+type Manush = {
+  name: string;
+  age: number;
+  address: {
+    city: "Dhaka";
+    road: "10 ali";
+    home?: "";
+  };
+};
+
+const manush: Manush = {
+  name: "Nayim",
+  age: 30,
+  address: {
+    city: "Dhaka",
+    road: "10 ali",
+  },
+};
+
+const home = manush?.address?.home ?? "No Home";
+
+console.log(home);
