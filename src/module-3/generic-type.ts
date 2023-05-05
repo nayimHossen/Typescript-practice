db26827dfae3c3f78920ae24df9c4cca17001376
Nayim Hossen <nayimh2015@gmail.com>
2023-05-05 14:32:35 +0600

    generic-type

---
+++ src/module-3/generic-type.ts
@@ -0,0 +1,30 @@
+// type GenericTuple<x, y> = [x, y];
+
+// const relation: GenericTuple<string, string> = ["nayim", "hossen"];
+
+// // type RelationWithSalaryType = { name: string; salary: number };
+
+// interface RelationWithSalaryInterface {
+//   name: string;
+//   salary: number;
+// }
+
+// const relationWithSalary: GenericTuple<RelationWithSalaryInterface, string> = [
+//   { name: "Nayim", salary: 10000 },
+//   "Sultana",
+// ];
+
+// type GenericArray<T> = Array<T>;
+
+// const rollNumber2: GenericArray<number> = [2, 3, 4, 5, 6];
+
+// const rollNumber3: GenericArray<string> = ["1", "2", "4"];
+
+// const rollNumber4: GenericArray<boolean> = [true, false];
+
+// type NameRollType = { name: string; roll: number };
+
+// const userNameAndRoll: Array<NameRollType> = [
+//   { name: "Nayim", roll: 23 },
+//   { name: "Hossen", roll: 40 },
+// ];
