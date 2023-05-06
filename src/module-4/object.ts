ee19a228ae42caf764256d2cc9f7fd09dd5f29a0
Nayim Hossen <nayimh2015@gmail.com>
2023-05-07 00:02:50 +0600

    inheritence in typescript

--- src/module-4/object.ts
+++ src/module-4/object.ts
@@ -1,19 +1,19 @@
-class Animal {
-  constructor(
-    public name: string,
-    public species: string,
-    public sound: string
-  ) {}
+// class Animal {
+//   constructor(
+//     public name: string,
+//     public species: string,
+//     public sound: string
+//   ) {}
 
-  public makeSound() {
-    console.log(`The ${this.name} says ${this.sound} `);
-  }
-}
+//   public makeSound() {
+//     console.log(`The ${this.name} says ${this.sound} `);
+//   }
+// }
 
-const dog = new Animal("Tome", "Dog", "Ghu Ghu");
-const cat = new Animal("Love", "Cat", "Mau Mau");
+// const dog = new Animal("Tome", "Dog", "Ghu Ghu");
+// const cat = new Animal("Love", "Cat", "Mau Mau");
 
-dog.makeSound();
-cat.makeSound();
+// dog.makeSound();
+// cat.makeSound();
 
-cat.name = "spicla";
+// cat.name = "spicla";
