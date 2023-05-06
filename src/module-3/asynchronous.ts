bac1dcab3e06a4cc3a39704e05c8f82e41077af6
Nayim Hossen <nayimh2015@gmail.com>
2023-05-06 19:19:31 +0600

    asynchronous typescript

--- src/module-3/asynchronous.ts
+++ src/module-3/asynchronous.ts
@@ -1,4 +1,4 @@
-//json placeholder api
+//json placeholder api https://jsonplaceholder.typicode.com/todos/1
 
 interface ITodo {
   userId: number;
@@ -8,25 +8,23 @@ interface ITodo {
 }
 
 const getTodo = async (): Promise<ITodo> => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1
-    `);
-  return await response.json();
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+
+  return await res.json();
 };
 
-const getTodoData = async (): Promise<void> => {
+const getTodoData = async () => {
   const result = await getTodo();
   console.log(result);
 };
 
-getTodoData();
-
 const makePromise = (): Promise<string> => {
   return new Promise<string>((resolve, reject) => {
-    const data = "Data is fetched";
+    const data: string = "data is fetched";
     if (data) {
       resolve(data);
     } else {
-      reject("failed to fetch data");
+      reject("Failed to fetch data");
     }
   });
 };
@@ -37,7 +35,7 @@ const makePromiseBoolean = (): Promise<boolean> => {
     if (data) {
       resolve(data);
     } else {
-      reject("failed to fetch data");
+      reject("Failed to fetch data");
     }
   });
 };
@@ -46,13 +44,13 @@ type DataType = {
   data: string;
 };
 
-const makePromiseObject = (): Promise<object> => {
-  return new Promise<object>((resolve, reject) => {
-    const data: object = { data: "data" };
+const makePromiseObject = (): Promise<DataType> => {
+  return new Promise<DataType>((resolve, reject) => {
+    const data: DataType = { data: "hello object" };
     if (data) {
       resolve(data);
     } else {
-      reject("failed to fetch data");
+      reject("Failed to fetch data");
     }
   });
 };
@@ -67,7 +65,7 @@ const getPromiseDataBoolean = async (): Promise<boolean> => {
   return data;
 };
 
-const getPromiseDataObject = async (): Promise<object> => {
+const getPromiseDataObject = async (): Promise<DataType> => {
   const data = await makePromiseObject();
   return data;
 };
