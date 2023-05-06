8d84d618cd3e2a976518811c7416eb9b367a55a8
Nayim Hossen <nayimh2015@gmail.com>
2023-05-07 00:34:47 +0600

    type guard in type script

--- src/module-4/inheritence.ts
+++ src/module-4/inheritence.ts
@@ -1,38 +1,38 @@
-class Parent {
-  name: string;
-  age: number;
-  address: string;
+// class Parent {
+//   name: string;
+//   age: number;
+//   address: string;
 
-  constructor(name: string, age: number, address: string) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-  }
+//   constructor(name: string, age: number, address: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
 
-  makeSleep(hours: number): string {
-    return `This ${this.name} will sleep for ${hours}`;
-  }
-}
+//   makeSleep(hours: number): string {
+//     return `This ${this.name} will sleep for ${hours}`;
+//   }
+// }
 
-class Student extends Parent {
-  constructor(name: string, age: number, address: string) {
-    super(name, age, address);
-  }
-}
+// class Student extends Parent {
+//   constructor(name: string, age: number, address: string) {
+//     super(name, age, address);
+//   }
+// }
 
-const student1 = new Student("Nayim", 23, "Dhaka");
+// const student1 = new Student("Nayim", 23, "Dhaka");
 
-class Teacher extends Parent {
-  designation: string;
+// class Teacher extends Parent {
+//   designation: string;
 
-  constructor(name: string, age: number, address: string, designation: string) {
-    super(name, age, address);
-    this.designation = designation;
-  }
+//   constructor(name: string, age: number, address: string, designation: string) {
+//     super(name, age, address);
+//     this.designation = designation;
+//   }
 
-  takeClasses(numberOfClass: number): string {
-    return `This ${this.name} will take ${numberOfClass}`;
-  }
-}
+//   takeClasses(numberOfClass: number): string {
+//     return `This ${this.name} will take ${numberOfClass}`;
+//   }
+// }
 
-const teacher1 = new Teacher("Mr. Harun", 35, "USA", "Professor");
+// const teacher1 = new Teacher("Mr. Harun", 35, "USA", "Professor");
