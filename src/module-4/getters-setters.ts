ff8012a395ab621b12aedc2d2971715ce5f51c16
Nayim Hossen <nayimh2015@gmail.com>
2023-05-07 13:29:41 +0600

    static in class

--- src/module-4/getters-setters.ts
+++ src/module-4/getters-setters.ts
@@ -1,43 +1,43 @@
-class BankAccount {
-  public id: number;
-  public name: string;
-  private _balance: number;
+// class BankAccount {
+//   public id: number;
+//   public name: string;
+//   private _balance: number;
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this._balance = balance;
-  }
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this._balance = balance;
+//   }
 
-  //getter
-  get balance(): number {
-    return this._balance;
-  }
+//   //getter
+//   get balance(): number {
+//     return this._balance;
+//   }
 
-  //   getBalance(): number {
-  //     return this._balance;
-  //     // console.log(`My Current Balance is ${this._balance}`);
-  //   }
+//   //   getBalance(): number {
+//   //     return this._balance;
+//   //     // console.log(`My Current Balance is ${this._balance}`);
+//   //   }
 
-  set deposit(amount: number) {
-    this._balance = this._balance + amount;
-  }
+//   set deposit(amount: number) {
+//     this._balance = this._balance + amount;
+//   }
 
-  //   addDeposit(amount: number) {
-  //     this._balance = this._balance + amount;
-  //   }
-}
+//   //   addDeposit(amount: number) {
+//   //     this._balance = this._balance + amount;
+//   //   }
+// }
 
-class StudentAccount extends BankAccount {
-  test() {
-    // this._balance;
-  }
-}
+// class StudentAccount extends BankAccount {
+//   test() {
+//     // this._balance;
+//   }
+// }
 
-const myAccount = new BankAccount(33, "Nayim", 200);
-// myAccount.addDeposit(20);
-// myAccount.getBalance();
-// myAccount.getBalance();
-myAccount.deposit = 400;
+// const myAccount = new BankAccount(33, "Nayim", 200);
+// // myAccount.addDeposit(20);
+// // myAccount.getBalance();
+// // myAccount.getBalance();
+// myAccount.deposit = 400;
 
-console.log(myAccount.balance);
+// console.log(myAccount.balance);
