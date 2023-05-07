b16acde2989de9b9a703d38dcd0c5821def64e1e
Nayim Hossen <nayimh2015@gmail.com>
2023-05-07 13:08:39 +0600

    access-modifiers

--- src/module-4/type-guards.ts
+++ src/module-4/type-guards.ts
@@ -1,99 +1,99 @@
-//keyof guard
+// //keyof guard
 
-type AlphaNumerics = string | number;
+// type AlphaNumerics = string | number;
 
-function add(p: AlphaNumerics, p1: AlphaNumerics) {
-  if (typeof p === "number" && typeof p1 === "number") {
-    return p + p1;
-  } else {
-    return p.toString() + p1.toString();
-  }
-}
+// function add(p: AlphaNumerics, p1: AlphaNumerics) {
+//   if (typeof p === "number" && typeof p1 === "number") {
+//     return p + p1;
+//   } else {
+//     return p.toString() + p1.toString();
+//   }
+// }
 
-add(1, 3);
-add("1", "4");
+// add(1, 3);
+// add("1", "4");
 
-//In guard
+// //In guard
 
-type NormalUserType = {
-  name: string;
-};
+// type NormalUserType = {
+//   name: string;
+// };
 
-type AdminUserType = {
-  name: string;
-  role: "admin";
-};
+// type AdminUserType = {
+//   name: string;
+//   role: "admin";
+// };
 
-function getUser(user: NormalUserType | AdminUserType): string {
-  if ("role" in user) {
-    return `I am admin and my role is ${user.role}`;
-  } else {
-    return `I am normal user`;
-  }
-}
+// function getUser(user: NormalUserType | AdminUserType): string {
+//   if ("role" in user) {
+//     return `I am admin and my role is ${user.role}`;
+//   } else {
+//     return `I am normal user`;
+//   }
+// }
 
-const normalUser: NormalUserType = { name: "Nayim Hossen" };
+// const normalUser: NormalUserType = { name: "Nayim Hossen" };
 
-const adminUser: AdminUserType = { name: "sultana", role: "admin" };
+// const adminUser: AdminUserType = { name: "sultana", role: "admin" };
 
-console.log(getUser(normalUser));
-console.log(getUser(adminUser));
+// console.log(getUser(normalUser));
+// console.log(getUser(adminUser));
 
-//Instance of guard
+// //Instance of guard
 
-class Animal {
-  name: string;
-  species: string;
+// class Animal {
+//   name: string;
+//   species: string;
 
-  constructor(name: string, species: string) {
-    this.name = name;
-    this.species = species;
-  }
+//   constructor(name: string, species: string) {
+//     this.name = name;
+//     this.species = species;
+//   }
 
-  makeSound() {
-    console.log("I am making sound");
-  }
-}
+//   makeSound() {
+//     console.log("I am making sound");
+//   }
+// }
 
-class Dog extends Animal {
-  constructor(name: string, species: string) {
-    super(name, species);
-  }
-  makeBark() {
-    console.log(`I am Barking`);
-  }
-}
+// class Dog extends Animal {
+//   constructor(name: string, species: string) {
+//     super(name, species);
+//   }
+//   makeBark() {
+//     console.log(`I am Barking`);
+//   }
+// }
 
-class Cat extends Animal {
-  constructor(name: string, species: string) {
-    super(name, species);
-  }
+// class Cat extends Animal {
+//   constructor(name: string, species: string) {
+//     super(name, species);
+//   }
 
-  makeMew() {
-    console.log(`I am dancing`);
-  }
-}
+//   makeMew() {
+//     console.log(`I am dancing`);
+//   }
+// }
 
-function isDog(animal: Animal): animal is Dog {
-  return animal instanceof Dog;
-}
+// function isDog(animal: Animal): animal is Dog {
+//   return animal instanceof Dog;
+// }
 
-function isCat(animal: Animal): animal is Cat {
-  return animal instanceof Cat;
-}
+// function isCat(animal: Animal): animal is Cat {
+//   return animal instanceof Cat;
+// }
 
-function getAnimal(animal: Animal) {
-  if (isDog(animal)) {
-    animal.makeBark();
-  } else if (isCat(animal)) {
-    animal.makeMew();
-  } else {
-    animal1.makeSound();
-  }
-}
+// function getAnimal(animal: Animal) {
+//   if (isDog(animal)) {
+//     animal.makeBark();
+//   } else if (isCat(animal)) {
+//     animal.makeMew();
+//   } else {
+//     animal1.makeSound();
+//   }
+// }
 
-const animal1 = new Dog("Tome hero", "Dog");
-const animal2 = new Cat("Tome hero", "Cat");
+// const animal1 = new Dog("Tome hero", "Dog");
+// const animal2 = new Cat("Tome hero", "Cat");
 
-getAnimal(animal1);
-getAnimal(animal2);
+// getAnimal(animal1);
+// getAnimal(animal2);
